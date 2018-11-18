d9650d9f8aac5f8f471c1bbaa44d6492d6cd8f0c
Karol Swieca <swieca.karol@gmail.com>
2018-11-18 19:35:50 +0100

    added quote

--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -7,7 +7,9 @@ export default class Home extends Component {
       <div className='wrapper'>
         <div className='wrapper__centered'>
           <h3>Welcome to budget planer </h3>
-          <p>A budget tells us what we can't afford, but it doesn't keep us from buying it.</p>
+          <p classname='quote'>
+            "A budget tells us what we can't afford, but it doesn't keep us from buying it."
+          </p>
           <div className='wrapper__buttons'>
             <Link className='waves-effect waves-light btn cyan darken-3' to='/signIn'> Sign In </Link>
             <Link className='waves-effect waves-light btn cyan darken-3' to='/signUp'> Create Account </Link>
